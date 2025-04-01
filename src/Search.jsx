854909a3166f576ff6a26bdf52c677d10b8d9182
Christine Belzie <shecoder30@gmail.com>
2025-03-31 20:26:21 -0400

    a11y: make the input more accessible

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import { useGlobalContext } from './Context'
-
+import React from "react";
+import { useGlobalContext } from "./Context";
 
 const Search = () => {
-  const { query, searchFn, showPopularNews, setReadingMode, readingMode } = useGlobalContext();
-  return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-        }}>
-        <button
-          onClick={() => setReadingMode(!readingMode)}
-        >
-          {readingMode ? "Exit Reading Mode" : "Enter Reading Mode"}
-        </button>
-      </div>
-      <h1 className="heading">
-        <span className='subText'>T</span>ech
-        <span className='subText'>P</span>ulse
-      </h1>
+	const { query, searchFn, showPopularNews, setReadingMode, readingMode } =
+		useGlobalContext();
+	return (
+		<>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+				}}>
+				<button onClick={() => setReadingMode(!readingMode)}>
+					{readingMode ? "Exit Reading Mode" : "Enter Reading Mode"}
+				</button>
+			</div>
+			<h1 className="heading">
+				<span className="subText">T</span>ech
+				<span className="subText">P</span>ulse
+			</h1>
 
-      {!showPopularNews &&
-        <form>
-          <div>
-            <input type="text" placeholder="search here"
-              value={query}
-              onChange={(e) => searchFn(e.target.value)}
-            />
-          </div>
-        </form>
-      }
-    </>
+			{!showPopularNews && (
+				<form>
+					<div>
+						<label htmlFor="search">Search</label>
+						<input
+							id="search-input"
+							type="text"
+							placeholder="search here"
+							value={query}
+							onChange={(e) => searchFn(e.target.value)}
+						/>
+					</div>
+				</form>
+			)}
+		</>
+	);
+};
 
-
-  )
-}
-
-export default Search
+export default Search;
